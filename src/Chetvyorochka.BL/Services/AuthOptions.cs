25493251a54f32f457ef8967ec0b37a316aeee5a
BritvA <apan1998@yandex.ru>
2023-03-14 19:14:58 +0300

    Время жизни jwt токена увеличина до 600 сек

--- src/Chetvyorochka.BL/Services/AuthOptions.cs
+++ src/Chetvyorochka.BL/Services/AuthOptions.cs
@@ -8,7 +8,7 @@ namespace Chetvyorochka.BL.Services
         public const string ISSUER = "MyAuthServer"; // издатель токена
         public const string AUDIENCE = "MyAuthClient"; // потребитель токена
         const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
-        public const int LIFETIME = 60; // время жизни токена - 60 секунд
+        public const int LIFETIME = 600; // время жизни токена - 600 секунд
         public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
             new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
     }
